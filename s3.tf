7eebcb04fe67f3a07bacbd7a4060a46f88b09781
Leonardo Fraga <lafraga91@gmail.com>
2024-09-23 23:11:07 -0300

    Change state

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "food-cloud-terraform-remote-state"
+    bucket = "food-cloud-terraform-remote-state-copy"
     key    = "development/create-database-backend.tfstate"
     # region = var.aws_region
     # Variables doesn't work here :(
