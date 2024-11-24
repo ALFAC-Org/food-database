030a43502944d4c12891304e9e269049fead6a9f
Leonardo Fraga <lafraga91@gmail.com>
2024-11-24 16:43:47 -0300

    Add config to allow lab_role

--- food-produto-db/db.tf
+++ food-produto-db/db.tf
@@ -12,3 +12,21 @@ resource "aws_dynamodb_table" "food_produto_dynamodb" {
 	Name = "food_produto_dynamodb"
   }
 }
+
+resource "aws_iam_policy" "dynamodb_policy" {
+  name        = "DynamoDBPolicy"
+  description = "Policy for DynamoDB table food_produto"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.arn_aws_lab_role
+        }
+        Action   = "dynamodb:*"
+        Resource = "arn:aws:dynamodb:us-east-1:767398113061:table/food_produto" // Esse valor talvez possamos pegar dinamicamente
+      }
+    ]
+  })
+}
